409f17b24fb345cae825dff119889ad9ed5dd1db
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-11-02 16:57:40 -0800

    Remove script tags at base of index.html body, delete unnecessary console log statements, comment out code for future use in index.js
    co-authored-by: <thomasgrantglenn@mac.com>

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 window.addEventListener('load', (event) => {
-  console.log('page is fully loaded');
 
     // import { MIDI } from 'MIDI.js/src'
     MIDI = MIDI.MIDI
     
     // global.MIDI = MIDI
     startup()
-    
     const c2 = document.getElementById('c2');
     c2.addEventListener('click', playSound);
-    console.log(c2)
+
     
     async function startup() {
       await MIDI.autoconnect()
@@ -28,9 +26,9 @@ window.addEventListener('load', (event) => {
     
     
     async function playSound() {
-      const notes = ['c2']
+      // const notes = ['c2']
       let start = MIDI.currentTime
-      const note = notes[0]
+      // const note = notes[0]
       // MIDI.noteOn(0, note, 127, start) // velocity = volume, max is 128, start is used to schedule notes in the future
       MIDI.noteOn(0, "A0", 127, start);
       MIDI.noteOff(0, "A0", start + 1500) // Do we add a decay or delay here?
@@ -41,8 +39,6 @@ window.addEventListener('load', (event) => {
       // onProgress()
       // getSoundModule()
       // setSoundModule()
-      console.log
-      console.log("Play C2 sound");
     }
     
     // MIDI.SoundModule gets the sound module or returns null and console warning if you don't have a sound module
