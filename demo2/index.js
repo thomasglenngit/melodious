60cfe959176a438e9278d27bd2c71df5acf88cfa
Thomas Glenn <thomasgrantglenn@mac.com>
2020-11-04 15:49:37 -0700

    add sketch demo2 to application.
    co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

---
+++ demo2/index.js
@@ -0,0 +1,16 @@
+const sketch = global.sketch
+const Gesture = global.Gesture
+const dom = sketch.dom
+
+const $selectButton = dom.$('#select')
+
+Gesture.on($selectButton, 'click', () => {
+	console.log('select')
+	sketch.setTool('select')
+})
+
+const $pencilButton = dom.$('#pencil')
+Gesture.on($pencilButton, 'click', () => {
+	console.log('pencil')
+	sketch.setTool('pencil')
+})
