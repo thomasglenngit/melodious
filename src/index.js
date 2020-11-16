cd399cef7b2f72fe8689853e24311c29431d7dc8
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-11-16 12:37:15 -0800

    Prevent switching modes if user selects 'cancel' on confirmation. Co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- src/index.js
+++ src/index.js
@@ -81,8 +81,15 @@ window.addEventListener('load', (event) => {  // may want to remove...
       par1.classList.remove('toggleText2')
       par2.classList.add('toggleText2')
     } else {
-      par2.classList.remove('toggleText2')
-      par1.classList.add('toggleText2')
+      let result = confirm('Changing modes will delete your artwork. Click okay to switch modes or cancel to stay in free-play and save your work before switching.')
+      if(result) {
+        sketch.doc.reset()
+        par2.classList.remove('toggleText2')
+        par1.classList.add('toggleText2')
+      } else {
+        document.getElementById('free-play').checked = true;
+        document.getElementById('learn-melody').checked = false;
+      }
     }
   })
 
