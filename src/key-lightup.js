3d50541b17bd57e98635f0883903490612235eae
Thomas Glenn <thomasgrantglenn@mac.com>
2020-11-19 12:20:14 -0700

    add learn-melody event listener to add predictive note light up.
    co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/key-lightup.js
+++ src/key-lightup.js
@@ -1,8 +1,8 @@
   // when you press a key, it changes color.
-  export async function keyLightup(keyId) {
-    const change = document.getElementById(keyId)
-    change.classList.add('colorAdd')
-    setTimeout(function(){
-      change.classList.remove('colorAdd')
-    }, 1000)
-  }
+  // export async function keyLightup(keyId) {
+  //   const change = document.getElementById(keyId)
+  //   change.classList.add('colorAdd')
+  //   setTimeout(function(){
+  //     change.classList.remove('colorAdd')
+  //   }, 1000)
+  // }
