dbd6436c41c9431d0ede737b5d6c32d19231411e
Thomas Glenn <thomasgrantglenn@mac.com>
2020-11-20 11:38:31 -0700

    add font open sans.
    co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/learn-melody.js
+++ src/learn-melody.js
@@ -2,19 +2,6 @@ import './index'
 import { playSound } from './play-sound'
 import { showSyllable } from './show-syllable'
 
-//Procedure
-//Melody: 'Somewhere Over the Rainbow' in F major.
-// 'f2', 'f3', 'e3', 'c3', 'd3', 'e3', 'f3'
-//Define for each note: 1. Target note(currentKey); 2. light() 3. Sound(); 4. Shape() 5. Next note(nextKey); 6. Next light()
-//Play - to begin the melody (one event listener)
-//Correct Note Click - event listener for each correct note in the right order.
-
-//Step 1. Create an array of notes.
-//Step 2. Indicate the first note the array with a light.
-//Step 3. indicate the next note in the array with a light. 
-//Step 4. Repeat steps 3 and 4 until the end of the array
-
-//Melody starts with "learn-melody" radio button
 export async function learnMelody() {
   const melody = ['f2', 'f3', 'e3', 'c3', 'd3', 'e3', 'f3']
   const syllables = ['some', 'where', 'o', 'ver', 'the', 'rain', 'bow']
@@ -51,55 +38,4 @@ export async function learnMelody() {
       console.log('Something went wrong')
     }
   })
-
-  // if(note === melody.length - 1) {
-  //   console.log('Yay, you made it!')
-  // }
 }
-  
-
-
-
-  // export async function nextKeyLight(next) {
-  //   next = melody[note + 1]
-  //   next.classList.add('colorAdd')
-  // }
-
-  // export async function sleep(ms) {
-  //   return new Promise(resolve => setTimeout(resolve, ms));
-  // }
-
-  // export async function melodyLoop() {
-  //   for (const note of melody) {
-  //     await keyLight(note)
-  //     await sleep(500)
-  //     if (currentKey === note) {
-  //       await playSound(note)
-  //       await nextKeyLight(next)
-  //       await emitSketchLayers(note)
-  //     }
-  //     note++
-  //   }
-  // }
-
-  // export async function asyncForEach(array, callback) {
-  //   for (let index = 0; index < array.length; index++) {
-  //     await callback(array[index], index, array);
-  //   }
-  // }
-
-
-
-    // await playSound(note)
-    // await keyLight(note)
-    // await nextKeyLight(note)
-    // await emitSketchLayers(note)
-    // await sleep(200)
-
-// array = [1,2,3,4,5,6]
-
-//   for(const number of array) {
-//   let nextNumber = array[number + 1]
-//    console.log(number)
-//    console.log(nextNumber)
-// }
--- src/show-syllable.js
+++ src/show-syllable.js
@@ -1,25 +1,20 @@
 export async function showSyllable(syllable){
   const doc = sketch.doc
-  sketch.tools.text.config({
-    content: syllable,
-    fontFamily: 'Freckle Face'
-  })
-  // sketch.setTool('text').then(tool => {
-  //   await doc.addLayer(tool)
-  // })
-  // await doc.addLayer({
 
-  // })
+  await doc.addLayer({
+    type: 'fancyText',  // [shapes[A]]
+    fontFamily: 'Open Sans',
+    fill: 'charteuse',
+    textAlign: 'center',
+    fontSize: 50,
+    content: 'What is up?',
+    x: 0,
+    y: 0
+
+  })
   // sketch.setTool('text')
   // await doc.setTool('')
   console.log(syllable);
+  
+    
 }
-
-// sketch.tools.[toolname].config()
-// sketch.tools.text.config({params params})
-// don't need to pass in anything we don't want to change by default
-// similar to state
-
-// sketch.setTool(type).then(tool => {
-
-// })
