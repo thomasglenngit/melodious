922a007bf1cfd54231743a9ec7bc750577517d1c
Thomas Glenn <thomasgrantglenn@mac.com>
2020-11-23 13:04:27 -0700

    add coordinate increments to showSyllable function.
    co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/show-syllable.js
+++ src/show-syllable.js
@@ -1,17 +1,23 @@
 export async function showSyllable(syllable){
   const doc = sketch.doc
-
+  let layers = sketch.layers
   await doc.addLayer({
     type: 'fancyText',  // [shapes[A]]
     fontFamily: 'freckle-face',
     fill: '#000',
     textAlign: 'center',
-    fontSize: 100,
+    fontSize: 50,
     content: `${syllable}`,
     x: 200,
     y: 30,
-    opacity: 1
+    opacity: 1,
   })
+  layers.forEach(function(layer){
+    layer.x -= 50,
+    layer.y += 30,
+    sketch.doc.render
+  })
+  
 }
 
 
