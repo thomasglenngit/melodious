6e8b1e81f13205fbe85ac52bff72f5a330d877a4
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-11-25 10:39:50 -0800

    Add getMelody function, separate rainbow melody from learn melody file.
    Co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

---
+++ src/melodies/rainbow.js
@@ -0,0 +1,131 @@
+/**
+   * @typedef {Object} MelodyNote
+   * @property {string} keyToHighlight
+   * @property {string} syllable
+   * @property {string} note
+   */
+  /**
+   * @typedef {getMelody} function 
+   */
+
+  export function getMelody(melody) {
+    if(melody === 'rainbow') {
+      return [
+        {
+          keyToHighlight: 'f2',
+          syllable: 'Some',
+          noteID: 0
+        },
+        {
+          keyToHighlight: 'f3',
+          syllable: 'where',
+          noteID: 1
+        },
+        {
+          keyToHighlight: 'e3',
+          syllable: 'o',
+          noteID: 2
+        },
+        {
+          keyToHighlight: 'c3',
+          syllable: 'ver',
+          noteID: 3
+        },
+        {
+          keyToHighlight: 'd3',
+          syllable: 'the',
+          noteID: 4
+        },
+        {
+          keyToHighlight: 'e3',
+          syllable: 'rain',
+          noteID: 5
+        },
+        {
+          keyToHighlight: 'f3',
+          syllable: 'bow',
+          noteID: 6
+        },
+        {
+          keyToHighlight: 'f2',
+          syllable: 'way',
+          noteID: 7
+        },
+        {
+          keyToHighlight: 'd3',
+          syllable: 'up',
+          noteID: 8
+        },
+        {
+          keyToHighlight: 'c3',
+          syllable: 'high',
+          noteID: 9
+        },
+        {
+          keyToHighlight: 'd2',
+          syllable: 'there\'s',
+          noteID: 10
+        },
+        {
+          keyToHighlight: 'as3',
+          syllable: 'a',
+          noteID: 11
+        },
+        {
+          keyToHighlight: 'a3',
+          syllable: 'land',
+          noteID: 12
+        },
+        {
+          keyToHighlight: 'f2',
+          syllable: 'that',
+          noteID: 13
+        },
+        {
+          keyToHighlight: 'g2',
+          syllable: 'I',
+          noteID: 14
+        },
+        {
+          keyToHighlight: 'a3',
+          syllable: 'dreamed',
+          noteID: 15
+        },
+        {
+          keyToHighlight: 'as3',
+          syllable: 'of',
+          noteID: 16
+        },
+        {
+          keyToHighlight: 'g2',
+          syllable: 'once',
+          noteID: 17
+        },
+        {
+          keyToHighlight: 'e2',
+          syllable: 'in',
+          noteID: 18
+        },
+        {
+          keyToHighlight: 'f2',
+          syllable: 'a',
+          noteID: 19
+        },
+        {
+          keyToHighlight: 'g2',
+          syllable: 'lul',
+          noteID: 20
+        },
+        {
+          keyToHighlight: 'a3',
+          syllable: 'la',
+          noteID: 21
+        },
+        {
+          keyToHighlight: 'f2',
+          syllable: 'by...',
+          noteID: 22
+        }
+      ]
+    }
+  }
