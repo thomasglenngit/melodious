6c7feef1a01176e1c3c1ab9a99a974fef852434e
Thomas Glenn <thomasgrantglenn@mac.com>
2020-11-24 22:59:21 -0700

    add 15 more objects to melody array in learn-melody.js.
    co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/learn-melody.js
+++ src/learn-melody.js
@@ -34,7 +34,7 @@ export async function learnMelody() {
     },
     {
       keyToHighlight: 'e3',
-      syllable: 'o',
+      syllable: 'o-',
       noteID: 2
     },
     {
@@ -49,13 +49,93 @@ export async function learnMelody() {
     },
     {
       keyToHighlight: 'e3',
-      syllable: 'rain',
+      syllable: 'rain-',
       noteID: 5
     },
     {
       keyToHighlight: 'f3',
       syllable: 'bow',
       noteID: 6
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'way',
+      noteID: 7
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'up',
+      noteID: 8
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'high',
+      noteID: 9
+    },
+    {
+      keyToHighlight: 'd2',
+      syllable: 'there\'s',
+      noteID: 10
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'a',
+      noteID: 11
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'land',
+      noteID: 12
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'that',
+      noteID: 13
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'I',
+      noteID: 14
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'dreamed',
+      noteID: 15
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'of',
+      noteID: 16
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'once',
+      noteID: 17
+    },
+    {
+      keyToHighlight: 'e2',
+      syllable: 'in',
+      noteID: 18
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'a',
+      noteID: 19
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'lul-',
+      noteID: 20
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'la-',
+      noteID: 21
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'by',
+      noteID: 22
     }
   ]
 
