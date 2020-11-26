84af1abcd75a1c4b8b818bfc3d0d798022d29c2d
Thomas Glenn <thomasgrantglenn@mac.com>
2020-11-25 21:41:54 -0700

    complete objects in xmas.js and respective script file.
    co-authored-by: Brittany Lindgren <brittdevelopsit@gmail.com>

--- src/melodies/xmas.js
+++ src/melodies/xmas.js
@@ -8,12 +8,698 @@
  * @typedef {getMelody} function 
  */
 
-export function getMelodyXmas() {
+function getMelodyXmas() {
   return [
     {
       keyToHighlight: 'f2',
       syllable: 'Chestnuts',
       noteID: 0
-    }
+    },
+    {
+      keyToHighlight: 'f3',
+      syllable: 'nuts',
+      noteID: 1
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'roast',
+      noteID: 2
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'ing',
+      noteID: 3
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'on',
+      noteID: 4
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'an',
+      noteID: 5
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'open fire',
+      noteID: 6
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'jack',
+      noteID: 7
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'frost',
+      noteID: 8
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'nipp',
+      noteID: 9
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'ing',
+      noteID: 10
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'at',
+      noteID: 11
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'your',
+      noteID: 12
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'nose,',
+      noteID: 13
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'yule',
+      noteID: 14
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'tide',
+      noteID: 15
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'carols',
+      noteID: 16
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'be',
+      noteID: 17
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'ing',
+      noteID: 18
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'sung',
+      noteID: 19
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'by',
+      noteID: 20
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'a',
+      noteID: 21
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'choir',
+      noteID: 22
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'and',
+      noteID: 23
+    },
+    {
+      keyToHighlight: 'cs3',
+      syllable: 'folks',
+      noteID: 24
+    },
+    {
+      keyToHighlight: 'cs3',
+      syllable: 'dressed',
+      noteID: 25
+    },
+    {
+      keyToHighlight: 'ds3',
+      syllable: 'up',
+      noteID: 26
+    },
+    {
+      keyToHighlight: 'cs3',
+      syllable: 'like',
+      noteID: 27
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'es',
+      noteID: 28
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'ki',
+      noteID: 29
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'mos',
+      noteID: 30
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'e',
+      noteID: 31
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'v\'ry',
+      noteID: 32
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'bo',
+      noteID: 33
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'dy',
+      noteID: 34
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'knows',
+      noteID: 35
+    },
+    {
+      keyToHighlight: 'f3',
+      syllable: 'a',
+      noteID: 36
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'tur',
+      noteID: 37
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'key',
+      noteID: 38
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'and',
+      noteID: 39
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'some',
+      noteID: 40
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'mistle',
+      noteID: 41
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'toe',
+      noteID: 42
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'help',
+      noteID: 43
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'to',
+      noteID: 44
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'make',
+      noteID: 45
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'the',
+      noteID: 46
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'sea',
+      noteID: 47
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'son',
+      noteID: 48
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'bright',
+      noteID: 49
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'tiny',
+      noteID: 50
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'tots',
+      noteID: 51
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'with',
+      noteID: 52
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'their',
+      noteID: 53
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'eyes',
+      noteID: 54
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'all',
+      noteID: 55
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'a',
+      noteID: 56
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'glow',
+      noteID: 57
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'will',
+      noteID: 58
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'find',
+      noteID: 59
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'it',
+      noteID: 60
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'hard',
+      noteID: 61
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'to',
+      noteID: 62
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'sleep',
+      noteID: 63
+    },
+    {
+      keyToHighlight: 'e2',
+      syllable: 'to',
+      noteID: 64
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'night',
+      noteID: 65
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'they',
+      noteID: 66
+    },
+    {
+      keyToHighlight: 'e2',
+      syllable: 'know',
+      noteID: 67
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'that',
+      noteID: 68
+    },
+    {
+      keyToHighlight: 'ds3',
+      syllable: 'San',
+      noteID: 69
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'ta\'s',
+      noteID: 70
+    },
+    {
+      keyToHighlight: 'c',
+      syllable: 'on',
+      noteID: 71
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'his',
+      noteID: 72
+    },
+    {
+      keyToHighlight: 'c',
+      syllable: 'way',
+      noteID: 73
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'he\'s',
+      noteID: 74
+    },
+    {
+      keyToHighlight: 'e2',
+      syllable: 'load',
+      noteID: 75
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'ed',
+      noteID: 76
+    },
+    {
+      keyToHighlight: 'ds3',
+      syllable: 'lots',
+      noteID: 77
+    },
+    {
+      keyToHighlight: 'd2',
+      syllable: 'of',
+      noteID: 78
+    },
+    {
+      keyToHighlight: 'ds3',
+      syllable: 'toys',
+      noteID: 79
+    },
+    {
+      keyToHighlight: 'd2',
+      syllable: 'and',
+      noteID: 80
+    },
+    {
+      keyToHighlight: 'ds3',
+      syllable: 'good',
+      noteID: 81
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'ies',
+      noteID: 82
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'on',
+      noteID: 83
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'his',
+      noteID: 84
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'sleigh',
+      noteID: 85
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'and',
+      noteID: 86
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'e',
+      noteID: 87
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 've\'ry',
+      noteID: 88
+    },
+    {
+      keyToHighlight: 'cs3',
+      syllable: 'mo',
+      noteID: 89
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'ther\'s',
+      noteID: 90
+    },
+    {
+      keyToHighlight: 'cs3',
+      syllable: 'child',
+      noteID: 91
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'is',
+      noteID: 92
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'gon',
+      noteID: 93
+    },
+    {
+      keyToHighlight: 'gs2',
+      syllable: 'na',
+      noteID: 94
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'spy',
+      noteID: 95
+    },
+    {
+      keyToHighlight: 'gs2',
+      syllable: 'to',
+      noteID: 96
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'see',
+      noteID: 97
+    },
+    {
+      keyToHighlight: 'gs2',
+      syllable: 'if',
+      noteID: 98
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'reindeer',
+      noteID: 99
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'really',
+      noteID: 100
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'know',
+      noteID: 101
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'how',
+      noteID: 102
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'to',
+      noteID: 103
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'fly',
+      noteID: 104
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'and',
+      noteID: 105
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'so',
+      noteID: 106
+    },
+    {
+      keyToHighlight: 'f3',
+      syllable: 'I\'m',
+      noteID: 107
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'off',
+      noteID: 108
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'er',
+      noteID: 109
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'ing',
+      noteID: 110
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'this',
+      noteID: 111
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'simple phrase',
+      noteID: 112
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'to',
+      noteID: 113
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'kids',
+      noteID: 114
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'from',
+      noteID: 115
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'one',
+      noteID: 116
+    },
+    {
+      keyToHighlight: 'as3',
+      syllable: 'to',
+      noteID: 117
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'nine-',
+      noteID: 118
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'ty',
+      noteID: 119
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'two',
+      noteID: 120
+    },
+    {
+      keyToHighlight: 'e2',
+      syllable: 'al',
+      noteID: 121
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'though',
+      noteID: 122
+    },
+    {
+      keyToHighlight: 'e2',
+      syllable: 'it\'s',
+      noteID: 123
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'been',
+      noteID: 124
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'said',
+      noteID: 125
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'ma',
+      noteID: 126
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'ny',
+      noteID: 127
+    },
+    {
+      keyToHighlight: 'a3',
+      syllable: 'times',
+      noteID: 128
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'ma',
+      noteID: 129
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'ny',
+      noteID: 130
+    },
+    {
+      keyToHighlight: 'e3',
+      syllable: 'ways',
+      noteID: 131
+    },
+    {
+      keyToHighlight: 'f3',
+      syllable: 'Mer',
+      noteID: 132
+    },
+    {
+      keyToHighlight: 'd3',
+      syllable: 'ry',
+      noteID: 133
+    },
+    {
+      keyToHighlight: 'c3',
+      syllable: 'Christ',
+      noteID: 134
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'mas',
+      noteID: 135
+    },
+    {
+      keyToHighlight: 'g2',
+      syllable: 'to',
+      noteID: 136
+    },
+    {
+      keyToHighlight: 'f2',
+      syllable: 'you',
+      noteID: 137
+    },
   ]
 }
+
